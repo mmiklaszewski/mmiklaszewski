245e72eae07e4090039407ee3fce96e830d4a65f
“Maciej <maciej.miklaszewski@edbms.pl>
2023-12-12 06:33:17 +0100

    translation skeleton

--- src/UI/Controller/IndexController.php
+++ src/UI/Controller/IndexController.php
@@ -3,12 +3,21 @@
 namespace App\UI\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 final class IndexController extends AbstractController
 {
-    #[Route(path: '/', name: 'index')]
-    public function index()
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'pl']);
+
+    }
+    #[Route('/{_locale<%app.supported_locales%>}/main', name: 'homepage')]
+    public function index(): Response
     {
         return $this->render('about_me/index.html.twig');
     }
